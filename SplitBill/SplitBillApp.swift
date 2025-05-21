4ef2b6f31acfe04adb1137945753223502eea863
Vlad Kramskoy <vkto5609@gmail.com>
2025-05-21 23:24:50 +0300

    Initial commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Vladislav Kramskoy on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
